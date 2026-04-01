e350f5279df18e07c462d2c357f479363f7c72ee
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2026-04-01 19:52:26 +0200

    feat: add Updatable component and integrate general settings UI with update handlers

---
+++ apps/config/frontend/src/lib/ui/components/Updatable.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    interface Props {
+        id: string;
+        label: string;
+        value: string;
+        onClickUpdate: (value: string) => void;
+    }
+
+    let {
+        id,
+        label,
+        value = $bindable(),
+        onClickUpdate = $bindable(),
+    }: Props = $props();
+</script>
+
+<div class="flex flex-row items-center">
+    <dt class="text-sm font-semibold w-56">{label}</dt>
+    <dd class="flex text-sm text-gray-700">
+        <span class="w-32 text-red-700 font-semibold">{value}</span>
+        <span class="ml-4 shrink-0">
+            <button
+                    onclick={() => onClickUpdate(value)}
+                    id="{id}-update"
+                    type="button"
+                    class="cursor-pointer bg-white font-semibold text-indigo-600 hover:text-indigo-500"
+            >Update</button>
+        </span>
+    </dd>
+</div>
--- apps/config/frontend/src/routes/+page.svelte
+++ apps/config/frontend/src/routes/+page.svelte
@@ -1,4 +1,35 @@
 <script lang="ts">
+    import Updatable from "$lib/ui/components/Updatable.svelte";
+
+    const updateDefaultLogbook = (value: string) => {
+        console.log(`Updating default logbook to ${value}`);
+    }
+    const updateDefaultRig = (value: string) => {
+        console.log(`Updating default rig to ${value}`);
+    }
+    const updateDefaultFreq = (value: string) => {
+        console.log(`Updating default frequency to ${value}`);
+    }
 </script>
 
-<p>Page</p>
+<div class="mx-8">
+    <div>
+        <h3>General Settings</h3>
+        <p>These settings cannot be empty. The all have a <i>default value</i>.</p>
+    </div>
+    <div class="mt-6 border-t border-gray-100 dark:border-white/10">
+        <dl class="divide-y divide-gray-100 dark:divide-white/10">
+            <Updatable id="default-logbook" label="Default Logbook ID" value="1" onClickUpdate={updateDefaultLogbook}/>
+        </dl>
+    </div>
+    <div class="mt-6 border-t border-gray-100 dark:border-white/10">
+        <dl class="divide-y divide-gray-100 dark:divide-white/10">
+            <Updatable id="default-rig" label="Default Rig ID" value="1" onClickUpdate={updateDefaultFreq}/>
+        </dl>
+    </div>
+    <div class="mt-6 border-t border-gray-100 dark:border-white/10">
+        <dl class="divide-y divide-gray-100 dark:divide-white/10">
+            <Updatable id="default-freq" label="Default Frequency" value="14.600.000" onClickUpdate={updateDefaultRig}/>
+        </dl>
+    </div>
+</div>
