48f2ca9f98da9c616c5cf20cbdc2b8e366d6ba1d
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2026-04-04 09:29:12 +0200

    feat: implement configuration state management and error handling in layout loading

--- apps/config/frontend/src/routes/+layout.ts
+++ apps/config/frontend/src/routes/+layout.ts
@@ -1,6 +1,22 @@
+import {GetConfig} from "$lib/wailsjs/go/facade/Service";
+import {LogError} from "$lib/wailsjs/runtime";
+import {types} from "$lib/wailsjs/go/models";
+import {configState} from "$lib/states/config-state.svelte";
+
 export const prerender = true;
 export const ssr = false;
 
 export const load = async (): Promise<void> => {
+    try {
+        let cfg: types.AppConfig | null | undefined = await GetConfig();
+        if (!cfg) {
+            const msg = 'GetConfig() returned null or undefined; using defaults.';
+            LogError(msg);
+            cfg = new types.AppConfig();
+        }
 
+        configState.load(cfg);
+    } catch (e: unknown) {
+
+    }
 }
