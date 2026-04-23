0b0fdf383c512923bb087b78f7f6d22e5af2f0d5
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2026-04-23 06:35:51 +0200

    Add `cmd/logging` app: define window initialization, event handling loop, and update `.gitignore`.

--- cmd/logging/main.go
+++ cmd/logging/main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
 
 	"gioui.org/app"
+	giop "gioui.org/op"
+	"gioui.org/unit"
+
+	"github.com/ColonelBlimp/station-manager/internal/errors"
+)
+
+const (
+	windowTitle  = "Station Manager — Logging"
+	windowWidth  = unit.Dp(1024)
+	windowHeight = unit.Dp(751)
 )
 
 func main() {
-
 	go func() {
 		window := new(app.Window)
+		window.Option(
+			app.Title(windowTitle),
+			app.Size(windowWidth, windowHeight),
+		)
 		if err := run(window); err != nil {
+			log.Print(err)
 			os.Exit(1)
 		}
 		os.Exit(0)
@@ -19,9 +33,19 @@ func main() {
 	app.Main()
 }
 
-func run(window *app.Window) (err error) {
+func run(window *app.Window) error {
+	const op errors.Op = "logging.app.main.run"
 	if window == nil {
-		return fmt.Errorf("window cannot be nil")
+		return errors.New(op).WithMsg("window cannot be nil")
 	}
-	return nil
+	var ops giop.Ops
+	for {
+		switch e := window.Event().(type) {
+		case app.DestroyEvent:
+			return errors.New(op).WithErr(e.Err)
+		case app.FrameEvent:
+			gtx := app.NewContext(&ops, e)
+			e.Frame(gtx.Ops)
+		}
+	}
 }
