aab48f40c6d785eda90f37075cd6198a04ae2cba
ColonelBlimp <20266202+ColonelBlimp@users.noreply.github.com>
2026-05-01 06:14:09 +0200

    Add `.editorconfig` for consistent code formatting, fix indentation in `app.svelte`, and add a placeholder `favicon.ico`.

--- frontend/logging/src/app.svelte
+++ frontend/logging/src/app.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-  import LoggingCard from "./lib/ui/cards/LoggingCard.svelte";
+    import LoggingCard from "./lib/ui/cards/LoggingCard.svelte";
 </script>
 
 <main class="rounded-xl border border-gray-200 h-120 w-200 mx-auto mt-10">
-  <LoggingCard/>
+    <LoggingCard/>
 </main>
